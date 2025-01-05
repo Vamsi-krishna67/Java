f8b236a1aaa4155154d9a7a41d5f55da0110057b
Vamsi-krishna67 <91657490+Vamsi-krishna67@users.noreply.github.com>
2025-01-05 12:33:55 +0530

    Triangle formations using some logics
    In this the code and output of different patterns have been written respectively and commented everything to get rid of error. While executing the code uncomment the particular code accordingly to get the respective pattern output.

---
+++ .github/Java Codes/Patterns/LogicalTriangle.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class LogicalTriangle {
+
+	public static void main(String[] args) {
+    //35.print the pattern
+//		int n=1;
+//		for(int i=1;i<=5;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(n);
+//				n++;
+//				if(n==10) {
+//					n=1;
+//				}
+//			}
+//			System.out.println();
+//		}
+		
+		
+		//output:
+//				1
+//				23
+//				456
+//				7891
+//				23456
+		
+		
+		
+		
+		
+		//36. print the pattern
+//		for(int i=1;i<=6;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				1
+//				12
+//				123
+//				1234
+//				12345
+//				123456
+		
+		
+		
+		
+		//37. print the pattern
+//		int n=1;
+//		for(int i=1;i<=6;i++) {
+//			for(int j=1;j<=i;j++) {
+//				if(n%2==0) {
+//					System.out.print(0);
+//				}
+//				else {
+//					System.out.print(1);
+//				}
+//				
+//				n++;
+//				if(n==10) {
+//					n=1;
+//				}
+//				
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				1
+//				01
+//				010
+//				1011
+//				01010
+//				101101
+
+  }
+}
