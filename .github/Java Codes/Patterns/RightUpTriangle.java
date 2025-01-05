3f0a60083511e3c1cb6d6e79ee14ea5f6aacfcee
Vamsi-krishna67 <91657490+Vamsi-krishna67@users.noreply.github.com>
2025-01-05 12:02:27 +0530

    hypotenuse on right and up side on right angles triangle
    In this the code and output of different patterns have been written respectively and commented everything to get rid of error. While executing the code uncomment the particular code accordingly to get the respective pattern output.

---
+++ .github/Java Codes/Patterns/RightUpTriangle.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class RightUpTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. print the pattern
+//		for (int i=1;i<=5;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				1
+//				12
+//				123
+//				1234
+//				12345
+		
+		
+		//2. print the pattern
+//		for(int i=1;i<=5;i++) {
+//			for(int j =i;j>=1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				1
+//				21
+//				321
+//				4321
+//				54321
+		
+		
+		// 3. Print the pattern
+//		for(int i=5;i>=1;i--) {
+//			for (int j=5;j>=i;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		
+		//output:
+//				5
+//				54
+//				543
+//				5432
+//				54321
+
+		
+		//4. print the pattern:
+//		for(int i=5;i>=1;i--) {
+//			for(int j=i;j<=5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				5
+//				45
+//				345
+//				2345
+//				12345
+		
+		//5. Print the pattern
+		
+//		for(int i=5;i>=1;i--) {
+//		for(int j=i;j<=5;j++) {
+//			System.out.print("*");
+//		}
+//		System.out.println();
+//		}
+		
+		
+		//output
+//				*
+//				**
+//				***
+//				****
+//				*****
+
+  }
+}
