21e3fa5c85d6438b197cef04c14e183c7178d8ce
Vamsi-krishna67 <91657490+Vamsi-krishna67@users.noreply.github.com>
2025-01-05 11:53:05 +0530

    Square box pattern
    In this the code and output of different patterns of box model have been written respectively and commented everything to get rid of error. While executing the code uncomment the particular code accordingly to get the respective pattern output.

---
+++ .github/Java Codes/Patterns/box.java
@@ -0,0 +1,174 @@
+import java.util.Scanner;
+
+public class box {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// 1. print 12345 for 5 times line by line
+//		for(int i=1;i<=4;i++) {
+//			for(int j=1;j<=5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		
+		// output:
+//				12345
+//				12345
+//				12345
+//				12345
+
+		
+		
+		
+		//2. print the same as above in reverse order
+//		for(int i=1;i<=4;i++) {
+//			for(int j=5;j>=1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		// output:
+//				54321
+//				54321
+//				54321
+//				54321
+		
+		
+		// 3. print the pattern 11111,22222,333333,44444 in line by line
+		
+//		for(int i =1;i<=5;i++) {
+//			for (int j=1;j<=4;j++) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+		
+		// output:
+//				1111
+//				2222
+//				3333
+//				4444
+//				5555
+		
+		
+		
+		//4. print the same as above in reverese
+//		for(int i =5;i>=1;i--) {
+//			for (int j=1;j<=5;j++) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+		
+		// output:
+//				55555
+//				44444
+//				33333
+//				22222
+//				11111
+
+
+
+		//5. print the pattern:
+//		for (int i = 1; i <= 5; i++) {
+//            // Loop through columns
+//            for (int j = 1; j <= 5; j++) {    
+//            	if (j % 2 == 0) {
+//                    System.out.print("0");
+//                } 
+//            	else {
+//                    System.out.print("1");
+//                }
+//            }
+//            System.out.println();
+//		}
+		
+		//output:
+//				10101
+//				10101
+//				10101
+//				10101
+//				10101
+		
+		
+		//6. print the pattern
+//		for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= 5; j++) {    
+//            	if (i % 2 == 0) {
+//                    System.out.print("0");
+//                } 
+//            	else {
+//                    System.out.print("1");
+//                }
+//            }
+//            System.out.println();
+//		}
+		
+		//output:
+//				11111
+//				00000
+//				11111
+//				00000
+//				11111
+		
+		// 7. print the pattern
+//		for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= 5; j++) {    
+//            	if (i % 2 == 0) {
+//                    System.out.print("O");
+//                } 
+//            	else {
+//                    System.out.print("A");
+//                }
+//            }
+//            System.out.println();
+//		}
+		
+		//OUTPUT:
+//				AAAAA
+//				OOOOO
+//				AAAAA
+//				OOOOO
+//				AAAAA
+		
+		
+
+		//8. print the pattern
+//		for(char i='A';i<='E';i++) {
+//			for(char j='A';j<='E';j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				ABCDE
+//				ABCDE
+//				ABCDE
+//				ABCDE
+//				ABCDE
+		
+		
+		//9. print the pattern
+//		for(char i='A';i<='E';i++) {
+//			for(char j='A';j<='E';j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		//output:
+//		*****
+//		*****
+//		*****
+//		*****
+//		*****
+
+  }
+}
