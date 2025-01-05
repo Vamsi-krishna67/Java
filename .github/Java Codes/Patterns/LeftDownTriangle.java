929f1add0e9f5cbc05b099925d40343ee4d96179
Vamsi-krishna67 <91657490+Vamsi-krishna67@users.noreply.github.com>
2025-01-05 12:24:34 +0530

    Hypotenuse on left and down side of the triangle
    In this the code and output of different patterns have been written respectively and commented everything to get rid of error. While executing the code uncomment the particular code accordingly to get the respective pattern output.

---
+++ .github/Java Codes/Patterns/LeftDownTriangle.java
@@ -0,0 +1,143 @@
+import java.util.Scanner;
+
+public class LeftDownTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    //1. print the pattern
+//		for(int i=1;i<=5;i++) {
+//			for(int s=1;s<i;s++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=5;j>=i;j--) {
+//				System.out.print(j);
+//				
+//				
+//			
+//			}
+//			
+//			System.out.println();
+//		}
+		
+		//output:
+//				54321
+//				 5432
+//				  543
+//				   54
+//				    5
+		
+		
+		//2. print the pattern
+//		for(int i=5;i>=1;i--) {
+//			for(int s=5;s>i;s--) {
+//				System.out.print(" ");
+//			}
+//			for(int j=i;j>=1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+
+		//output:
+//				54321
+//				 4321
+//				  321
+//				   21
+//				    1
+		
+		
+		//3.print the pattern
+//		for(int i=5;i>=1;i--) {
+//			for (int s=i;s<5;s++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				12345
+//				 1234
+//				  123
+//				   12
+//				    1
+		
+		//4.print the pattern
+//		for(int i=1;i<=5;i++) {
+//			for(int s=1;s<i;s++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=i;j<=5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//				
+//		}
+		
+		//output:
+//				12345
+//				 2345
+//				  345
+//				   45
+//				    5
+		
+		
+		//5.print the pattern
+//		for(char i='E';i>='A';i--) {
+//			for(char s=i;s<'E';s++) {
+//				System.out.print(" ");
+//			}
+//			for(char j='A';j<=i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output
+//				ABCDE
+//				 ABCD
+//				  ABC
+//				   AB
+//				    A
+		
+		//6. print the pattern
+//		for(char i='A';i<='E';i++) {
+//			for(char s='A';s<i;s++) {
+//				System.out.print(" ");
+//			}
+//			for(char j=i;j<='E';j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				ABCDE
+//				 BCDE
+//				  CDE
+//				   DE
+//				    E
+
+    //7. print the pattern
+//		for(char i='A';i<='E';i++) {
+//			for(char s='A';s<i;s++) {
+//				System.out.print(" ");
+//			}
+//			for(char j=i;j<='E';j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+
+      //output:
+//				*****
+//				 ****
+//				  ***
+//				   **
+//				    *
+  }
+}
+  
