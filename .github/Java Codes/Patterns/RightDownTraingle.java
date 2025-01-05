95aa2b637e6d6aa5e9345756c4814bc1d871a80c
Vamsi-krishna67 <91657490+Vamsi-krishna67@users.noreply.github.com>
2025-01-05 12:15:08 +0530

    Hypotenuse on right and down side of the right angle triangle
    In this the code and output of different patterns have been written respectively and commented everything to get rid of error. While executing the code uncomment the particular code accordingly to get the respective pattern output.

---
+++ .github/Java Codes/Patterns/RightDownTraingle.java
@@ -0,0 +1,128 @@
+import java.util.Scanner;
+
+public class RightDownTraingle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+    // 1. print the pattern
+//		for (int i=1;i<=5;i++) {
+//			for(int j=5;j>=i;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//			
+//		}
+		
+		
+		//output:
+//				54321
+//				5432
+//				543
+//				54
+//				5
+
+		
+		
+		//2. print the pattern
+//		for(int i=5 ;i>=1 ;i--) {
+//			for (int j=1 ;j<=i ;j++ ) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				12345
+//				1234
+//				123
+//				12
+//				1
+		
+		
+		
+		
+		//3. print the pattern
+//		for(int i = 1;i<=5;i++) {
+//			for (int j=i;j<=5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		
+		//output:
+//				12345
+//				2345
+//				345
+//				45
+//				5
+		
+		//4. print the pattern
+//		for(int i=5;i>=1;i--) {
+//			for(int j=i;j>=1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+
+		//output:
+//				54321
+//				4321
+//				321
+//				21
+//				1
+
+		
+		
+		
+		//5. print the pattern
+		
+//		for(char i='E';i>='A' ;i--) {
+//			for (char j='A';j<=i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				ABCDE
+//				ABCD
+//				ABC
+//				AB
+//				A
+		
+		
+		
+		//6.print the pattern
+//		for(char i='A';i<='E';i++) {
+//			for (char j=i;j<='E';j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//output:
+//				ABCDE
+//				BCDE
+//				CDE
+//				DE
+//				E
+
+
+		
+		//7. print the pattern
+//		for(char i='A';i<='E';i++) {
+//			for (char j=i;j<='E';j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+    //output:
+//				*****
+//				****
+//				***
+//				**
+//				*
+
+  }
+}
